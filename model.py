564e1d8fc790ea41e39eac08b67c9faf375048e1
Shali <shaliselvakumar@gmail.com>
2026-04-16 23:53:06 +0400

    remove tensorflow fix deployment

--- model.py
+++ model.py
@@ -1,17 +1,6 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from sklearn.linear_model import LinearRegression
 
 def build_model():
 
-    model = Sequential()
-
-    model.add(Dense(32, activation='relu', input_shape=(2,)))
-    model.add(Dense(32, activation='relu'))
-    model.add(Dense(1))
-
-    model.compile(
-        optimizer='adam',
-        loss='mse'
-    )
-
+    model = LinearRegression()
     return model
--- train.py
+++ train.py
@@ -3,6 +3,6 @@ from model import build_model
 def train_model(X, y):
 
     model = build_model()
-    model.fit(X, y, epochs=30, batch_size=4, verbose=0)
+    model.fit(X, y)
 
     return model
